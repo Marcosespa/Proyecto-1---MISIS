a88c9ee02edf0504a87edccbcc43a6a2222d387b
Marcosespa <marcosespa24@gmail.com>
2025-02-18 10:34:13 -0500

    Pendiente

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from transformers import pipeline
 
 db = SQLAlchemy()
 jwt = JWTManager()
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'doc', 'md'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def create_app():
     app = Flask(__name__)
@@ -12,15 +18,16 @@ def create_app():
 
     app.config.from_object('app.config.Config')
     app.config['JWT_SECRET_KEY'] = 'super-secret-key' 
-
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     db.init_app(app)
     jwt.init_app(app)
-
-    # from app.routes import main_routes
+    global summarizer
+    summarizer = pipeline("summarization")
+    
     from app.auth import auth_routes 
-    # app.register_blueprint(main_routes)
-    app.register_blueprint(auth_routes)
+    from app.docs import doc_routes
 
+    app.register_blueprint(auth_routes)
 
     return app
