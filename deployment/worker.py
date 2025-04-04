7fc8ce70d05a62fb287e67bc4e732efa79d4ac22
Marcosespa <marcosespa24@gmail.com>
2025-04-03 19:43:35 -0500

    jeje

--- deployment/worker.py
+++ deployment/worker.py
@@ -9,10 +9,8 @@ while True:
     for filename in os.listdir(NFS_PATH):
         filepath = os.path.join(NFS_PATH, filename)
         if os.path.isfile(filepath) and not filename.endswith(".done"):
-            print(f"🔧 Procesando archivo: {filename}")
-            # Simula procesamiento
+            print(f"Procesando archivo: {filename}")
             time.sleep(2)
-            # Marca como procesado
             new_name = filepath + ".done"
             os.rename(filepath, new_name)
             print(f"✅ Procesado: {new_name}")
