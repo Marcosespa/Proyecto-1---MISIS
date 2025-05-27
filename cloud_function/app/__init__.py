80763506edd7fe87baae89217ca3f813e2d12028
Marcosespa <marcosespa24@gmail.com>
2025-05-27 18:46:45 -0500

    Cambios

---
+++ cloud_function/app/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask,jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from app.config import Config
+from app.database import db
+import os
+from flask_jwt_extended import jwt_required
+from dotenv import load_dotenv
+load_dotenv()
+
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    # CORS(app, resources={r"/*": {"origins": "*", "allow_headers": ["Authorization", "Content-Type"]}})
+    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
+    # Crear carpeta de uploads si no existe
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+    # Inicializar extensiones
+    db.init_app(app)
+    jwt.init_app(app)
+
+    @app.route('/')
+    def index():
+        return "API corriendo"
+
+    # Registrar Blueprints
+    from app.auth import auth_bp 
+    from app.docs import docs_bp, upload_file
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(docs_bp, url_prefix="/docs")
+
+    # @app.route('/upload', methods=['POST'])
+    # @jwt_required()
+    # def wrapped_upload_file():
+    #     return upload_file()
+    @app.route('/health')
+    def health():
+        return jsonify({'status': 'ok'})
+
+    return app
---
+++ cloud_function/app/models.py
@@ -0,0 +1,26 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    nombre_usuario = db.Column(db.String(64), unique=True, nullable=False, index=True)
+    contrasena = db.Column(db.String(512), nullable=False)
+    imagen_perfil = db.Column(db.String(256), nullable=True)
+    documentos = db.relationship("Documento", backref="usuario", lazy=True)
+
+
+    def set_password(self, password: str):
+        self.contrasena = generate_password_hash(password)
+
+    def check_password(self, password: str):
+        return check_password_hash(self.contrasena, password)
+
+class Documento(db.Model):
+    __tablename__ = 'documentos'
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+    filename = db.Column(db.String(256), nullable=False)
+    text = db.Column(db.Text, nullable=True)
+    summary = db.Column(db.Text, nullable=True)
+    status=db.Column(db.String(20),default="pending")
